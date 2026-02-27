41e71dd00d4d75cdc0f13fb179fc2368bc96e493
Jeremiah <sentomerojeremy@gmail.com>
2026-02-27 09:02:29 +0300

    ..

--- packages/ui/postcss.config.cjs
+++ packages/ui/postcss.config.cjs
@@ -1,6 +1,4 @@
 /* eslint-disable no-undef */
-/* eslint-env node */
-/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
